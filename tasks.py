cc1b75763999587984ebfcf17c4c5b3674bc7df6
Theodorescsa <dinhthai0312@gmail.com>
2024-10-03 18:12:40 +0700

    sss

--- tasks.py
+++ tasks.py
@@ -60,20 +60,25 @@ def redownload_video(url,res):
         urllib.request.urlretrieve(url, f'{VIDEOS_PATH}re-{id}')
         return {"title": "Non-YouTube Video", "id": f"{id}", "status": "completed"}
     else:
-        yt = pytube.YouTube(url)
+        yt = YouTube(url)
         yt_id = yt.video_id
-        stream = next(
-        (s for s in filter(lambda s: s.type == 'video', yt.fmt_streams) 
-         if get_resolution(s) == res), 
-        None
-        )
+        video_stream = yt.streams.filter(adaptive=True, file_extension='mp4', only_video=True, resolution=f'{res}p').first()        
+        audio_stream = yt.streams.filter(adaptive=True, file_extension='mp4', only_audio=True).first()
         
-        if stream:
-            stream.download(output_path=VIDEOS_PATH, filename="re-"+yt_id)
-            print(f"Đã tải xuống luồng video với độ phân giải {res}p.")
-        else:
-            print(f"Không tìm thấy luồng video với độ phân giải {res}p.")
-        return {"title": yt.title, "id": yt_id, "status": "completed"}
+        video_path = video_stream.download(filename=f'{yt_id}.mp4')
+        audio_path = audio_stream.download(filename=f"{AUDIOS_PATH}audio-{yt_id}.wav")
+        if video_stream:
+            print(f"Selected video resolution: {video_stream.resolution}")
+        # Merge using FFmpeg (ensure ffmpeg is installed and in the system path)
+        output_path = f'{VIDEOS_PATH}re-{yt_id}.mp4'
+        print(output_path)
+        os.system(f'ffmpeg -i {video_path} -i {audio_path} -c:v copy -c:a aac {output_path}')
+        os.rename(output_path, f'{VIDEOS_PATH}re-{yt_id}')
+        print(f"Video and audio merged into {output_path}.")
+       
+        # yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution().download(output_path=VIDEOS_PATH, filename=yt_id)
+        return {"title": yt.title, "id": yt_id, "status": "completed","audio":audio_path}
+    
     
 def convert_seconds(seconds):
     hours = seconds // 3600
